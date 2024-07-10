6d8f44f010ba006b2625e6e9786099d1546e806a
codebydaud <119681431+codebydaud@users.noreply.github.com>
2024-07-10 15:53:31 +0500

    Add Dependency Checks

--- book-api/src/main/java/com/codebydaud/training/lecture_02/book_api/news/News.java
+++ book-api/src/main/java/com/codebydaud/training/lecture_02/book_api/news/News.java
@@ -1,17 +1,17 @@
-package com.codebydaud.training.lecture_02.book_api.news;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@Entity
-public class News {
-    @Id
-    private Long id;
-    private String title;
-    private String author;
-    private String description;
-}
+//package com.codebydaud.training.lecture_02.book_api.news;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@Entity
+//public class News {
+//    @Id
+//    private Long id;
+//    private String title;
+//    private String author;
+//    private String description;
+//}
--- book-api/src/main/java/com/codebydaud/training/lecture_02/book_api/news/NewsController.java
+++ book-api/src/main/java/com/codebydaud/training/lecture_02/book_api/news/NewsController.java
@@ -1,20 +1,20 @@
-package com.codebydaud.training.lecture_02.book_api.news;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.CompletableFuture;
-
-@RestController
-public class NewsController {
-    @Autowired
-    public final NewsApiClient newsApiClient;
-    public NewsController(NewsApiClient newsApiClient) {
-        this.newsApiClient = newsApiClient;
-    }
-    @GetMapping("/json-data")
-    public CompletableFuture<String> getNews() {
-        return newsApiClient.fetchNewsFromApi();
-    }
-}
+//package com.codebydaud.training.lecture_02.book_api.news;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.concurrent.CompletableFuture;
+//
+//@RestController
+//public class NewsController {
+//    @Autowired
+//    public final NewsApiClient newsApiClient;
+//    public NewsController(NewsApiClient newsApiClient) {
+//        this.newsApiClient = newsApiClient;
+//    }
+//    @GetMapping("/json-data")
+//    public CompletableFuture<String> getNews() {
+//        return newsApiClient.fetchNewsFromApi();
+//    }
+//}
